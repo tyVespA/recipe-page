aea060badb3ca247e05815abeb367be7166152e5
tyVespA <migliavacca.marco08@gmail.com>
2024-09-15 23:14:06 +0200

    Add fonts, add preparation-time-container

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,20 @@ function App() {
         combines beaten eggs cooked to perfection, optionally filled with your
         choice of cheese, vegetables, or meats.
       </p>
+      <div className="preparation-time-container">
+        <h3>Preparation time</h3>
+        <ul>
+          <li>
+            <strong>Total</strong>: Approximately 10 minutes
+          </li>
+          <li>
+            <strong>Preparation</strong>: 5 minutes
+          </li>
+          <li>
+            <strong>Cooking</strong>: 5 minutes
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
