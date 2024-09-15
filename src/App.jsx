8e1a8e8063156c96de7e2ebf2328103abdd58d15
tyVespA <migliavacca.marco08@gmail.com>
2024-09-15 22:51:36 +0200

    Add images, set up main container, add content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <>front</>;
+  return (
+    <div className="recipe-container">
+      <img src="/images/image-omelette.jpeg" alt="omlette picture" />
+      <h1>Simple Omelette Recipe</h1>
+      <p>
+        An easy and quick dish, perfect for any meal. This classic omelette
+        combines beaten eggs cooked to perfection, optionally filled with your
+        choice of cheese, vegetables, or meats.
+      </p>
+    </div>
+  );
 }
 
 export default App;
